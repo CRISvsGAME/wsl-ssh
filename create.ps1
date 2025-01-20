5e731438c04f346ca61943e8f2740912e892cbe6
cris <cris@crisvsgame.com>
2025-01-20 09:56:32 +0000

    WSL SSH 0.0.4
    Add Firewall Rule

--- create.ps1
+++ create.ps1
@@ -18,6 +18,53 @@ try {
         Write-Host "Firewall Rule Variables Not Set in '$ConfigFile'"
         exit 1
     }
+
+    $NetFirewallRule = Get-NetFirewallRule -Name $Env:Name -ErrorAction SilentlyContinue
+
+    if ($NetFirewallRule) {
+        $NetFirewallPortFilter = $NetFirewallRule | Get-NetFirewallPortFilter
+
+        if ($NetFirewallRule.DisplayName -ne $Env:DisplayName) {
+            Set-NetFirewallRule -Name $Env:Name -DisplayName $Env:DisplayName
+            Write-Host "Updated DisplayName to '$Env:DisplayName'"
+        }
+
+        if ($NetFirewallRule.Action -ne $Env:Action) {
+            Set-NetFirewallRule -Name $Env:Name -Action $Env:Action
+            Write-Host "Updated Action to '$Env:Action'"
+        }
+
+        if ($NetFirewallRule.Direction -ne $Env:Direction) {
+            Set-NetFirewallRule -Name $Env:Name -Direction $Env:Direction
+            Write-Host "Updated Direction to '$Env:Direction'"
+        }
+
+        if ($NetFirewallRule.Enabled -ne $Env:Enabled) {
+            Set-NetFirewallRule -Name $Env:Name -Enabled $Env:Enabled
+            Write-Host "Updated Enabled to '$Env:Enabled'"
+        }
+
+        if ($NetFirewallPortFilter.LocalPort -ne $Env:LocalPort) {
+            Set-NetFirewallRule -Name $Env:Name -LocalPort $Env:LocalPort
+            Write-Host "Updated LocalPort to '$Env:LocalPort'"
+        }
+
+        if ($NetFirewallPortFilter.Protocol -ne $Env:Protocol) {
+            Set-NetFirewallRule -Name $Env:Name -Protocol $Env:Protocol
+            Write-Host "Updated Protocol to '$Env:Protocol'"
+        }
+
+        Write-Host "Firewall Rule '$Env:Name' Configured Correctly"
+    }
+    else {
+        Write-Host "Firewall Rule '$Env:Name' Not Found. Creating..."
+
+        New-NetFirewallRule -Name "$Env:Name" -DisplayName "$Env:DisplayName" `
+            -Action "$Env:Action" -Direction "$Env:Direction" -Enabled "$Env:Enabled" `
+            -LocalPort "$Env:LocalPort" -Protocol "$Env:Protocol"
+
+        Write-Host "Firewall Rule '$Env:Name' Created Successfully"
+    }
 }
 catch {
     Write-Host "Create Error: $($PsItem.Exception.Message)"
